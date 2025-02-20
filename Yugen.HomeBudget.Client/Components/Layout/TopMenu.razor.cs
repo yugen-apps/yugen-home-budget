25979d2aa74a0e3d281a36410086ea00f9f6261d
Emiliano Magliocca <emiliano.magliocca@outlook.com>
2025-02-20 16:04:44 +0100

    Fix UI

--- Yugen.HomeBudget.Client/Components/Layout/TopMenu.razor.cs
+++ Yugen.HomeBudget.Client/Components/Layout/TopMenu.razor.cs
@@ -1,13 +1,19 @@
 ﻿using Blazorise.Localization;
 using Microsoft.AspNetCore.Components;
 using Yugen.HomeBudget.Client.Models;
+using Yugen.HomeBudget.Client.Services;
 
 namespace Yugen.HomeBudget.Client.Components.Layout
 {
     public partial class TopMenu
     {
-        [Parameter]
-        public ForwardRef TargetForwardRef { get; set; }
+        private string? _identityName;
+
+        private string? _image = "assets/images/logo.png";
+
+        [CascadingParameter] protected Theme? Theme { get; set; }
+
+        [Parameter] public ForwardRef TargetForwardRef { get; set; }
 
         [Parameter] public EventCallback<string> ThemeColorChanged { get; set; }
 
@@ -19,11 +25,22 @@ namespace Yugen.HomeBudget.Client.Components.Layout
 
         [Inject] protected ITextLocalizerService? LocalizationService { get; set; }
 
-        [CascadingParameter] protected Theme? Theme { get; set; }
+        [Inject] private CustomStateProvider? AuthStateProvider { get; set; }
+
+        [Inject] private NavigationManager? NavigationManager { get; set; }
 
         protected override async Task OnInitializedAsync()
         {
             await SelectCulture("en-US");
+
+            if (AuthStateProvider != null)
+            {
+                var authenticationState = await AuthStateProvider.GetAuthenticationStateAsync();
+                _identityName = authenticationState?.User.Identity?.Name;
+
+                //CurrentUser? currentUser = await AuthStateProvider.GetCurrentUser();
+                //_identityName = currentUser?.UserName;
+            }
 
             await base.OnInitializedAsync();
         }
@@ -33,6 +50,20 @@ namespace Yugen.HomeBudget.Client.Components.Layout
             LocalizationService!.ChangeLanguage(name);
 
             return Task.CompletedTask;
+        }
+
+        private async Task BeginLogOut()
+        {
+            if (AuthStateProvider != null)
+            {
+                await AuthStateProvider.Logout();
+            }
+            NavigationManager?.NavigateTo("/");
+        }
+
+        private async Task NavigateTo(string url)
+        {
+            NavigationManager?.NavigateTo(url);
         }
     }
 }
