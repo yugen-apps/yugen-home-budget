4240739402520068694a180fe8876ba25e99703d
Emiliano Magliocca <emiliano.magliocca@outlook.com>
2025-07-28 08:59:22 +0200

    fix

--- Yugen.HomeBudget.Server/Program.cs
+++ Yugen.HomeBudget.Server/Program.cs
@@ -45,7 +45,7 @@ builder.Services.AddScoped<CustomStateProvider>();
 builder.Services.AddScoped<AuthenticationStateProvider>(s => s.GetRequiredService<CustomStateProvider>());
 builder.Services.AddScoped<IAuthService, AuthService>();
 
-builder.Services.AddHttpClient("Yugen.HomeBudget.ServerAPI", client => client.BaseAddress = new Uri("https://localhost:7288/"));
+builder.Services.AddHttpClient("Yugen.HomeBudget.ServerAPI", client => client.BaseAddress = new Uri(baseAddress));
 // Supply HttpClient instances that include access tokens when making requests to the server project
 builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>()
                                    .CreateClient("Yugen.HomeBudget.ServerAPI"));
